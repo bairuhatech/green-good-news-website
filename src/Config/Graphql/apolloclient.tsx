7596a6bdd104f96467f5d35e2384b87a6cbcee92
Bairuha Tech <92435692+bairuhatech@users.noreply.github.com>
2023-10-01 16:39:44 +0530

    Update apolloclient.tsx

--- src/Config/Graphql/apolloclient.tsx
+++ src/Config/Graphql/apolloclient.tsx
@@ -6,8 +6,8 @@ import {
   } from "@apollo/client";
   
   const client = new ApolloClient({
-    // uri: "https://suprabhaatham-dev.herokuapp.com/graphql",
-    uri: "https://green-good-news-server-9a460dab0f63.herokuapp.com/graphql",
+    uri: "https://suprabhaatham-dev.herokuapp.com/graphql",
+    // uri: "https://green-good-news-server-9a460dab0f63.herokuapp.com/graphql",
     cache: new InMemoryCache(),
   });
   
